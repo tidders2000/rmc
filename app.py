dd7fdb748a181e9e3d3e8ae17516b254353deed0
Simon Tidman <simon.tid@btinternet.com>
2019-03-18 09:54:54 +0000

    initial commit template with data connection

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 import os
-from flask import Flask
+import pymysql
+from flask import Flask, render_template, redirect, request, url_for, request
+
+
+
 app = Flask(__name__)
+
+username = os.getenv('C9_USER')
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='crowd')
 
 
 @app.route('/')
-def hello():
-    return "Hello World"
+@app.route('/ammend_user')
+def get_tasks():
+    
+    try:
+    # Run a query
+     with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+        sql = " SELECT users.id, users.firstname, users.lastname,location.locationname,teamname.teamname FROM users INNER JOIN location ON location.id=users.id INNER JOIN teamname ON teamname.id=users.teamid"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        
+    except:
+    # Close the connection, regardless of whether or not the above was successful
+     print("An exception occurred")
+    
+    
+    return render_template("ammend_user.html", users=result)
+
     
     
 if __name__=='__main__':
